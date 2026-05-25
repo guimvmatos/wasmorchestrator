1350a8b0d4f3b894399719f7dccf89020e8b5be8
Guilherme Matos <guilherme.matos@ericsson.com>
2026-05-25 13:48:56 +0200

    v2

--- Distributed/receivers/negative/src/lib.rs
+++ Distributed/receivers/negative/src/lib.rs
@@ -60,21 +60,8 @@ fn handle_client(mut stream: TcpStream, initialized: &mut i32) -> std::io::Resul
         let receiveduration = start_receive.elapsed().as_millis();
 
         let start_exec = Instant::now();
-        println!(
-            "TEST1: request={}",
-            input_img.request
-        );
 
         let mut status = bindings::planner::negativeworld::plan::invert(&input_img);
-        println!(
-            "TEST2: request={}",
-            input_img.request
-        );
-
-        println!(
-            "TEST3: request={}",
-            status.request
-        );
 
         let execduration = start_exec.elapsed().as_millis();
 
